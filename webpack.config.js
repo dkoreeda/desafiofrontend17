c52903e70042c7b80d257ec61be53e6e5ff0eaae
Deise Koreeda <deise.koreeda@gmail.com>
2017-06-19 22:58:54 -0400

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  }
+}
+
+module.exports = config;
